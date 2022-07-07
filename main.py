0b7626de59ce873ff83f58d7b3c3e463cc1fc0c8
Joel Nicolaj Pedersen <nicolajlpedersen@gmail.com>
2022-07-07 15:22:23 +0200

    Code reduced
    Had made a grave mistake and used readline() method instead of read() which means that i got the data in a list and not a string. Furthermore this meant that every bit of data had to be looped and stored in a new list and every first item in the list had to be changed and sewn back together.
    TLDR: I had made it a hell for myself because I used the wrong function!

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-# TODO: Create a letter using starting_letter.txt
-all_names = []
-l1 = []
-with open("./Input/Letters/starting_letter.txt", "r") as lines:
-    letter = lines.readlines()
+# # TODO: Create a letter using starting_letter.txt
+# all_names = []
+# l1 = []
+# with open("./Input/Letters/starting_letter.txt", "r") as lines:
+#     letter = lines.readlines()
+#
+# # for each name in invited_names.txt
+# with open("./Input/Names/invited_names.txt") as file:
+#     for name in file:
+#         x = name.strip("\n")
+#         all_names.append(x)
+#
+# for i in range(len(all_names)):
+#     # Replace the [name] placeholder with the actual name.
+#     r = letter[0].replace("[name]", all_names[i])
+#     l1.append(r)
+#
+# i = 0
+# for n in l1:
+#     letter[0] = n
+#     with open(f"./Output/ReadyToSend/{all_names[i]}", mode="w") as file:
+#         for v in letter:
+#             file.write(v)
+#     i += 1
 
-# for each name in invited_names.txt
-with open("./Input/Names/invited_names.txt") as file:
-    for name in file:
-        x = name.strip("\n")
-        all_names.append(x)
+with open("./Input/Names/invited_names.txt") as names:
+    all_names = names.readlines()
 
-for i in range(len(all_names)):
-    # Replace the [name] placeholder with the actual name.
-    r = letter[0].replace("[name]", all_names[i])
-    l1.append(r)
-
-i = 0
-for n in l1:
-    letter[0] = n
-    with open(f"./Output/ReadyToSend/{all_names[i]}", mode="w") as file:
-        for v in letter:
-            file.write(v)
-    i += 1
+with open("./Input/Letters/starting_letter.txt") as letter:
+    letter_content = letter.read()
+    for name in all_names:
+        stripped_name = name.strip()
+        final_letter = letter_content.replace("[name]", stripped_name)
+        with open(f"./Output/ReadyToSend/{stripped_name}", mode="w") as file:
+            file.write(final_letter)
